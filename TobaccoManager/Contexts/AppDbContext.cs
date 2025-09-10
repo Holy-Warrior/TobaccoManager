8d0169853bdca218bf8bdf78900621256e77553c
Holy-Warrior <hamzazafarnokia@gmail.com>
2025-09-10 03:06:20 -0700

    added DbContext for all models

---
+++ TobaccoManager/Contexts/AppDbContext.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using TobaccoManager.Models;
+
+namespace TobaccoManager.Contexts
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Stock> Stocks { get; set; }
+        public DbSet<Bundle> Bundles { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=tobacco_manager.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Stock>()
+                .HasMany(s => s.Bundles)
+                .WithOne()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Bundle>()
+                .Property(b => b.LeafGrade)
+                .HasConversion<string>();
+        }
+    }
+}
